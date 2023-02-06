eb660637a461eac2b613846094fc570d131e110d
Mark Andreev <mark.andreev@gmail.com>
2023-02-06 18:55:51 +0000

    Add tolist convert in do predict

--- predict_pipeline.py
+++ predict_pipeline.py
@@ -1,16 +1,18 @@
 import timeit
-import pandas as pd
 
+import pandas as pd
 from sklearn.datasets import fetch_openml
 
 from train import train
+
 
 def load_dataset():
     X, y = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True, parser='liac-arff')
     return X
 
+
 def do_predict(pipeline, X):
-    return pipeline.predict_proba(X[['pclass', 'sex', 'age', 'fare']])
+    return pipeline.predict_proba(X[['pclass', 'sex', 'age', 'fare']])[:, 0].astype(float).tolist()
 
 
 def py_benchmark():
@@ -22,4 +24,4 @@ def py_benchmark():
 
     t = timeit.Timer(lambda: evaluate_predict())
     out = t.repeat(repeat=100, number=1)
-    print((pd.DataFrame({'t, milliseconds': out})*1000).describe())
+    print((pd.DataFrame({'t, milliseconds': out}) * 1000).describe())
